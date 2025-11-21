6aa76b132a8d60fa8c440a33cebe405329d8bcdb
dmurgueitio <makerii@hotmail.com>
2025-11-20 20:12:35 -0500

    examen daniela

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,21 @@
 // src/firebase.js
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore } from 'firebase/firestore'; // Importa Firestore
+// Opcional: Si no usas analytics, puedes quitar esta línea
+// import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "examen-ba756.firebaseapp.com",
-  projectId: "examen-ba756",
-  storageBucket: "examen-ba756.appspot.com",
-  messagingSenderId: "TU_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyDfUV3NIz_G2D5emRfMYl7ri_nVBossssI",
+  authDomain: "mi-app-react-bd115.firebaseapp.com",
+  projectId: "mi-app-react-bd115",
+  storageBucket: "mi-app-react-bd115.firebasestorage.app",
+  messagingSenderId: "911769131030",
+  appId: "1:911769131030:web:e931d277f185a76f3c7d37",
+  measurementId: "G-JCGXC9RVX9"
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const db = getFirestore(app); // Inicializa Firestore y obtén la instancia
+// const analytics = getAnalytics(app); // Opcional
 
-export { app, db };
+export { app, db }; // Exporta app y db
